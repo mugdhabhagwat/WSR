15a8c48fc02c42d0ede41d0bc434ba2b0b18ff7c
Priyanka <phattale@cisco.com>
2016-03-31 11:35:55 +0530

    Login page checked-in

--- src/main/java/com/wsr/util/WSRConstants.java
+++ src/main/java/com/wsr/util/WSRConstants.java
@@ -2,6 +2,6 @@ package com.wsr.util;
 
 public interface WSRConstants {
 
-	public static final String GET_LOGIN_DETAILS = "SELECT  pu.user_id, pu.password, pr.role_description FROM projectusers pu,projectuserroles pru, projectroles pr WHERE pu.user_id=pru.user_id AND pr.role_id=pru.`role_id` AND pu.username=:userName";
+	public static final String GET_LOGIN_DETAILS = "SELECT  pu.user_id, pu.password, pr.role_description FROM projectusers pu,projectuserroles pru, projectroles pr WHERE pu.user_id=pru.user_id AND pr.role_id=pru.role_id AND pu.username=:userName";
 
 }
